5ffc521c1ec0a69cd86c4670eb286c9ebc3ddec3
Klaudia Brzezińska <79053101+brzezinskaKlaudia@users.noreply.github.com>
2023-09-01 16:23:28 +0200

    Update greedy.R
    code refactor

--- Exercise_6/greedy.R
+++ Exercise_6/greedy.R
@@ -1,102 +1,64 @@
-dna1 < -"GGCGTTCAGGCA"
-dna2 < -"AAGAATCAGTCA"
-dna3 < -"CAAGGAGTTCGC"
-dna4 < -"CACGTCAATCAC"
-dna5 < -"CAATAATATTCG"
+# DNA sequence definitions dna motifs
+dna1 <- "GGCGTTCAGGCA"
+dna2 <- "AAGAATCAGTCA"
+dna3 <- "CAAGGAGTTCGC"
+dna4 <- "CACGTCAATCAC"
+dna5 <- "CAATAATATTCG"
 
-# dna_all <- "GGCGTTCAGGCAAAGAATCAGTCACAAGGAGTTCGCCACGTCAATCACCAATAATATTCG"
-create_profile_matrix < - function(dna)
-{
+# Combining all sequences into one
+dna_all <- paste(dna1, dna2, dna3, dna4, dna5, sep = "")
 
-}
-dna_all < - "GACTATGGCACGAACATCGACGTACGCAAC"
-dna_stringSplit < - strsplit(dna_all, "")[[1]]
-# 1. create a dna matrix
-dna_matrix < - matrix(dna_stringSplit, nrow=5, ncol=6,
-                      byrow=TRUE)
-# 2. create a profile matrix, kmers=3
-profile_matrix < - dna_matrix[, c(1, 2, 3)]  # this is greedy
-profile_df < - as.data.frame(profile_matrix)
-# 2.1 Create a count matrix; we should know how many A,C,G,T...
-lvls < - c("G", "A", "C", "T")
-freq < - sapply(profile_df,
-                function(x)
-table(factor(x, levels=lvls,
-             ordered=TRUE)))
-# 2.2 Normalisation
-# N=value/total
-profile_normalized < - sweep(freq, 2, colSums(freq), FUN="/")
+# Step 1: Creating a matrix from DNA sequences
+dna_matrix <- matrix(unlist(strsplit(dna_all, "")), nrow = 5, ncol = 6, byrow = TRUE)
 
-# 3. Selecting the kmer(motif1) with highest score from first row
-high_score < - function(kmer_dna)
-{  # GTC 
-    kmer_dna_split < - strsplit(kmer_dna, "")
-print("kmer_dna_split")
-print(kmer_dna_split)
-print("kmer_dna")
-print(kmer_dna)
-score < - 1
-best_score < - 0
-for (i in 1:3){
-    sub_score < - 0
-# for(j in 1:4){
-print("kmer_dna_split[i]")
-print(kmer_dna_split[i])
-print(i)
-if (kmer_dna_split[i] == "G"){sub_score < - profile_normalized[1, i]}
-if (kmer_dna_split[i] == "A"){sub_score < - profile_normalized[2, i]}
-if (kmer_dna_split[i] == "C"){sub_score < - profile_normalized[3, i]}
-if (kmer_dna_split[i] == "T"){sub_score < - profile_normalized[4, i]}
-# }
+# Step 2: Creation of the profile matrix (for k = 3)
+profile_matrix <- dna_matrix[, c(1, 2, 3)]
+profile_df <- as.data.frame(profile_matrix)
 
-print("sub_score")
-print(sub_score)
+# Step 2.1: Creation of a counting matrix
+lvls <- c("G", "A", "C", "T")
+freq <- sapply(profile_df, function(x) table(factor(x, levels = lvls, ordered = TRUE)))
 
-score < - 1 * sub_score * score
-print("score")
-print(score)
+# Step 2.2: Standardisation
+profile_normalized <- sweep(freq, 2, colSums(freq), FUN = "/")
 
-}
-list_score < - c(score, kmer_dna)
-print("list_score")
-print(list_score[1])
-print(list_score[2:4])
-print("score")
-return (list_score)
-}
-best_score < - 0
-dna_score < - ""
-for (i in 1:10){
-
-    print("dna_matrix")
-    print(dna_matrix)
-    print(i)
-    end_of_range < - i + 2
-    print(end_of_range)
-    # print(dna_matrix[i, c(i:end_of_range)])
-    # print(high_score(dna_matrix[1,c(i:end_of_range)]))
-    score < - high_score(dna_matrix[1, c(i:end_of_range)])
-    print("score in function")
-    print(score)
-    if (best_score < score[1]){
-    # print("bestScore2")
-    # print(best_score) 
-    # print(score)
-    best_score < - score[1]
-    dna_score < - score[2:4]
-    }
-    print("bestScore")
-    print(best_score)
-    print(dna_score)
-    # print(score$score)
-    # print(score$kmer_dna)
+# Step 3: Function to calculate the best result for a given k-mer
+high_score <- function(kmer_dna) {
+  kmer_dna_split <- strsplit(kmer_dna, "")[[1]]
+  score <- 1
+  best_score <- 0
+  
+  for (i in 1:3) {
+    sub_score <- 0
+    
+    if (kmer_dna_split[i] == "G") sub_score <- profile_normalized[1, i]
+    if (kmer_dna_split[i] == "A") sub_score <- profile_normalized[2, i]
+    if (kmer_dna_split[i] == "C") sub_score <- profile_normalized[3, i]
+    if (kmer_dna_split[i] == "T") sub_score <- profile_normalized[4, i]
+    
+    score <- 1 * sub_score * score
+  }
+  
+  list_score <- c(score, kmer_dna)
+  return(list_score)
 }
 
-# 4. Looking for the bestscore
-print("bestScore")
+# Step 4: Calculation of the best result
+best_score <- 0
+dna_score <- ""
+
+for (i in 1:10) {
+  end_of_range <- i + 2
+  score <- high_score(paste(dna_matrix[1, c(i:end_of_range)], collapse = ""))
+  
+  if (best_score < score[1]) {
+    best_score <- score[1]
+    dna_score <- score[2:4]
+  }
+}
+
+# Step 5: Writing out the best score and k-means
+print("The best score:")
 print(best_score)
-print(kmer_dna)
-
-# 5. Validate Motif
-motiv_high_score < - high_score(dna_score)
-print(motiv_high_score)
+print("The best k-mer:")
+print(dna_score)
